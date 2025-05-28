915e679ff262a8834ec4d2bd61bd6fdfb683be93
Bruno Bastos Guimaraes <bruno.guimaraes@serpro.gov.br>
2025-05-28 09:36:06 -0300

    Increased supported version range

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.github.brunobastosg"
-version = "2.1.2"
+version = "2.1.3"
 
 repositories {
     mavenCentral()
@@ -29,7 +29,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("231")
-        untilBuild.set("251.*")
+        untilBuild.set("252.*")
         changeNotes = """
             Increased supported version range"""
     }
